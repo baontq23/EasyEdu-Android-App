20d6c891b0b82e14374a58d101d8e29de7c78c6c
baontq23 <baooshacker@gmail.com>
2022-11-08 03:00:59 +0700

    feat: initial commit

---
+++ app/src/main/java/com/btcdteam/easyedu/fragments/auth/LoginFragment.java
@@ -0,0 +1,22 @@
+package com.btcdteam.easyedu.fragments.auth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.btcdteam.easyedu.R;
+
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
---
+++ app/src/main/java/com/btcdteam/easyedu/models/Student.java
@@ -0,0 +1,63 @@
+package com.btcdteam.easyedu.models;
+
+import java.util.UUID;
+
+public class Student {
+    private String id;
+    private String name;
+    private String gender;
+    private String dob;
+    private String parentId;
+
+    public Student(String name, String gender, String dob, String parentId) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.gender = gender;
+        this.dob = dob;
+        this.parentId = parentId;
+    }
+
+    public Student() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+}
