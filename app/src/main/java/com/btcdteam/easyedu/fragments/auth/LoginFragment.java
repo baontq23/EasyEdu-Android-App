4188555ba26144cc7d061c0d8157eb9985e137dd
baontq23 <baooshacker@gmail.com>
2022-11-09 21:19:42 +0700

    feat: google sign in

--- app/src/main/java/com/btcdteam/easyedu/fragments/auth/LoginFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/auth/LoginFragment.java
@@ -1,29 +1,48 @@
 package com.btcdteam.easyedu.fragments.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.btcdteam.easyedu.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.kongzue.dialogx.dialogs.MessageDialog;
+import com.kongzue.dialogx.interfaces.OnDialogButtonClickListener;
 
 
 public class LoginFragment extends Fragment {
+    private static final String TAG = "LoginFragment";
     TextInputLayout inputLayoutLoginPhoneNumber, inputLayoutLoginPassword;
     TextInputEditText edLoginPhoneNumber, edLoginPassword;
     Button btnLogin, btnLoginGoogle;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
@@ -44,7 +63,50 @@ public class LoginFragment extends Fragment {
         });
 
         btnLoginGoogle.setOnClickListener(v -> {
-
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+            if (acct != null) {
+                new MessageDialog("Thông tin", "Email: " + acct.getEmail(), "Logout", "Close").setOkButtonClickListener(new OnDialogButtonClickListener<MessageDialog>() {
+                    @Override
+                    public boolean onClick(MessageDialog dialog, View v) {
+                        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(requireContext(), "Logout successfully!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        return false;
+                    }
+                }).show();
+            } else {
+                signInWithGoogle();
+            }
         });
+    }
+
+    private final ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getData() == null) {
+                //no data present
+                return;
+            }
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+            handleSignInResult(task);
+        }
+    });
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        loginLauncher.launch(signInIntent);
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(requireContext(), "Email: " + account.getEmail(), Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 }
