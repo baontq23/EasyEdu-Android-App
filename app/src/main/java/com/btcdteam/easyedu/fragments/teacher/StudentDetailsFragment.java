050d85074d03cae434ebea40295b440cb6c53cfc
suhdo <dungvipnr@gmail.com>
2022-11-15 12:21:57 +0700

    feat: student_detail

---
+++ app/src/main/java/com/btcdteam/easyedu/fragments/teacher/StudentDetailsFragment.java
@@ -0,0 +1,29 @@
+package com.btcdteam.easyedu.fragments.teacher;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.btcdteam.easyedu.R;
+
+public class StudentDetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
