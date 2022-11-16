07d974ef0389d6529845235790a749cef4471d99
TUAN <tuannaph21878@fpt.edu.vb>
2022-11-16 18:20:03 +0700

    feat: new methods and attribute of student and parent , viewClass

--- app/src/main/java/com/btcdteam/easyedu/fragments/teacher/subfragment/ParentFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/teacher/subfragment/ParentFragment.java
@@ -1,31 +1,40 @@
 package com.btcdteam.easyedu.fragments.teacher.subfragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.btcdteam.easyedu.R;
 import com.btcdteam.easyedu.adapter.ParentAdapter;
-import com.btcdteam.easyedu.interfaces.IonClick;
+import com.btcdteam.easyedu.apis.ServerAPI;
 import com.btcdteam.easyedu.models.Parent;
+import com.btcdteam.easyedu.network.APIService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParentFragment extends Fragment {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ParentFragment extends Fragment implements ParentAdapter.ParentItemListener {
     RecyclerView rcv;
     ParentAdapter adapter;
-    List<Parent> list = new ArrayList<>();
+    List<Parent> list;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,41 +46,53 @@ public class ParentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         // data test
-        list.add(new Parent("abc", "0123"));
-        list.add(new Parent("xyz", "0123"));
-        list.add(new Parent("hgfhgf", "0123"));
-        list.add(new Parent("uoiu", "0123"));
-        list.add(new Parent("eww", "0123"));
-        list.add(new Parent("arew", "0123"));
-
-        IonClick ionClickBtnOption = new IonClick() {
-            @Override
-            public void onClick(Object object) {
-                Parent parent = (Parent) object;
-                Toast.makeText(getContext(), "Clicked: "+ parent.getName(), Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        IonClick ionClickItem = new IonClick() {
-            @Override
-            public void onClick(Object object) {
-                Navigation.findNavController(requireActivity(), R.id.nav_host_teacher).navigate(R.id.action_classInfoFragment_to_studentDetailsFragment);
-            }
-        };
-
-        IonClick ionClickHeader = new IonClick() {
-            @Override
-            public void onClick(Object object) {
-
-            }
-        };
         rcv = view.findViewById(R.id.rcv_parent);
-        adapter = new ParentAdapter(list, ionClickItem, ionClickBtnOption, ionClickHeader);
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         rcv.setLayoutManager(manager);
-        rcv.setAdapter(adapter);
+    }
+
+    private void getListParent() {
+        SharedPreferences preferences = requireContext().getSharedPreferences("CLASSROOM_ID", Context.MODE_PRIVATE);
+        int classroomId = preferences.getInt("classroomId", 0);
+
+        retrofit2.Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).getListParentByIdClassRoom(classroomId);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(retrofit2.Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 200) {
+                    Type type = new TypeToken<List<Parent>>() {
+                    }.getType();
+                    list = new ArrayList<>();
+                    list = new Gson().fromJson(response.body().getAsJsonArray("data"), type);
+                    adapter = new ParentAdapter(list, ParentFragment.this);
+                    rcv.setAdapter(adapter);
+
+                } else {
+                    Toast.makeText(requireContext(), "Không có phụ huynh", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(requireContext(), "Lỗi kết nối tới máy chủ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getListParent();
+    }
+
+    @Override
+    public void onItemClick(int position, Parent parent) {
+
+    }
+
+    @Override
+    public void onOptionClick(int position, Parent parent) {
 
     }
 }
--- app/src/main/java/com/btcdteam/easyedu/fragments/teacher/subfragment/StudentFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/teacher/subfragment/StudentFragment.java
@@ -1,30 +1,84 @@
 package com.btcdteam.easyedu.fragments.teacher.subfragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.btcdteam.easyedu.R;
 import com.btcdteam.easyedu.adapter.StudentAdapter;
-import com.btcdteam.easyedu.interfaces.IonClick;
+import com.btcdteam.easyedu.apis.ServerAPI;
 import com.btcdteam.easyedu.models.Student;
+import com.btcdteam.easyedu.models.StudentDetail;
+import com.btcdteam.easyedu.network.APIService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.kongzue.dialogx.dialogs.BottomMenu;
+import com.kongzue.dialogx.interfaces.OnMenuItemClickListener;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentFragment extends Fragment {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class StudentFragment extends Fragment implements StudentAdapter.StudentItemListener {
     RecyclerView rcv;
     StudentAdapter adapter;
     List<Student> list = new ArrayList<>();
+    List<StudentDetail> studentDetailList = new ArrayList<>();
+    List<StudentDetail> studentDetailLis01 = new ArrayList<>();
+    SearchView searchView;
+    String text = null;
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle.getSerializable("Array") == null) {
+
+            } else {
+                studentDetailList = (List<StudentDetail>) bundle.getSerializable("Array");
+                adapter = new StudentAdapter(studentDetailList, StudentFragment.this);
+                LinearLayoutManager manager = new GridLayoutManager(getContext(), 1);
+                rcv.setLayoutManager(manager);
+                rcv.setAdapter(adapter);
+            }
+
+        }
+    };
+
+    BroadcastReceiver receiverName = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getStringExtra("search") != null) {
+                text = intent.getStringExtra("search").trim();
+                searchView.setQuery(text, true);
+            } else {
+                Toast.makeText(context, "null", Toast.LENGTH_SHORT).show();
+                searchView.setQuery(null, true);
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,33 +90,102 @@ public class StudentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
-        //data test
-        list.add(new Student("fdsaf fad", "nam", "24/3/2002", "1"));
-        list.add(new Student("sa fad", "nam", "24/3/2002", "1"));
-        list.add(new Student("ds fad", "nam", "24/3/2002", "1"));
-        list.add(new Student("hj fad", "nam", "24/3/2002", "1"));
-
-        IonClick headerOnclick = new IonClick() {
-            @Override
-            public void onClick(Object object) {
-
-            }
-        };
-
-        IonClick itemStudentOnclick = new IonClick() {
-            @Override
-            public void onClick(Object object) {
-                Navigation.findNavController(requireActivity(), R.id.nav_host_teacher).navigate(R.id.action_classInfoFragment_to_studentDetailsFragment);
-            }
-        };
-
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(receiver, new IntentFilter("ACTION"));
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(receiverName, new IntentFilter("SEARCH"));
         rcv = view.findViewById(R.id.rcv_student);
-        adapter = new StudentAdapter(list, itemStudentOnclick, headerOnclick);
-        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        searchView = view.findViewById(R.id.reSearchView);
+        getListStudent();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
 
-        rcv.setLayoutManager(manager);
-        rcv.setAdapter(adapter);
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter("");
+                return false;
+            }
+        });
+
+    }
+
+    private void getListStudent() {
+        SharedPreferences preferences = requireContext().getSharedPreferences("CLASSROOM_ID", Context.MODE_PRIVATE);
+        int classroomId = preferences.getInt("classroomId", 0);
+
+        Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).getListStudentByIdClassRoom(classroomId);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 200) {
+                    Type type = new TypeToken<List<StudentDetail>>() {
+                    }.getType();
+                    studentDetailList = new ArrayList<>();
+                    studentDetailLis01 = new ArrayList<>();
+                    studentDetailList = new Gson().fromJson(response.body().getAsJsonArray("data"), type);
+                    for (StudentDetail o : studentDetailList) {
+                        if (o.getSemester() % 2 != 0) {
+                            studentDetailLis01.add(o);
+                        }
+                    }
+                    adapter = new StudentAdapter(studentDetailLis01, StudentFragment.this);
+                    LinearLayoutManager manager = new GridLayoutManager(getContext(), 1);
+                    rcv.setLayoutManager(manager);
+                    rcv.setAdapter(adapter);
+                } else {
+                    Toast.makeText(requireContext(), "Vui lòng thêm học sinh", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(requireContext(), "Lỗi kết nối tới máy chủ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(int position, StudentDetail student) {
+        Bundle bundle = new Bundle();
+        bundle.putString("studentId", student.getStudentId());
+        bundle.putInt("classRoomId", student.getClassroomId());
+        Navigation.findNavController(requireActivity(), R.id.nav_host_teacher).navigate(R.id.action_classInfoFragment_to_studentDetailsFragment, bundle);
+    }
+
+    void deleteStudent(int position, String id) {
+        Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).deleteStudentById(id);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 204) {
+                    Toast.makeText(requireContext(), "Xóa thành công", Toast.LENGTH_SHORT).show();
+                    adapter.notifyItemRemoved(position);
+                } else {
+                    Toast.makeText(requireContext(), "Không tìm thấy thông tin học sinh", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(requireContext(), "Lỗi kết nối tới máy chủ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onOptionClick(int position, StudentDetail student) {
+        BottomMenu.show(new String[]{"Cập nhật", "Xóa"})
+                .setMessage(student.getName())
+                .setOnMenuItemClickListener(new OnMenuItemClickListener<BottomMenu>() {
+                    @Override
+                    public boolean onClick(BottomMenu dialog, CharSequence text, int index) {
+                        if (index == 1) {
+                            deleteStudent(position, student.getStudentId());
+                        }
+                        return false;
+                    }
+                });
     }
 }
