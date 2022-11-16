02c8ff4ebf90d5a1fa2cb27c81cc38c84f50ecbf
TUAN <tuannaph21878@fpt.edu.vb>
2022-11-16 18:12:26 +0700

    new methods of ClassInfoFragment

--- app/src/main/java/com/btcdteam/easyedu/fragments/teacher/ClassInfoFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/teacher/ClassInfoFragment.java
@@ -2,22 +2,13 @@ package com.btcdteam.easyedu.fragments.teacher;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuItemCompat;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
-
-
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,19 +22,45 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.navigation.Navigation;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.btcdteam.easyedu.R;
 import com.btcdteam.easyedu.adapter.ViewPagerAdapter;
+import com.btcdteam.easyedu.apis.ServerAPI;
+import com.btcdteam.easyedu.models.StudentDetail;
+import com.btcdteam.easyedu.network.APIService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class ClassInfoFragment extends Fragment {
     private View bgStudent, bgParent;
     private FrameLayout layoutStudent, layoutParent;
     private ViewPager2 viewPager2;
-
-    Toolbar toolbar, searchToolbar;
+    Toolbar toolbar, searchToolbar, toolbar02;
     Menu search_menu;
     MenuItem item_search;
     ImageView icSearch, icSetting;
+    List<StudentDetail> studentDetailLis01, studentDetailLis02, studentDetailList;
 
     ScaleAnimation scaleUpAnimation = new ScaleAnimation(0f, 1.0f, 1f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
     ScaleAnimation scaleDownAnimation = new ScaleAnimation(1f, 0f, 1f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
@@ -58,6 +75,7 @@ public class ClassInfoFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        getListStudent();
         bgStudent = view.findViewById(R.id.bg_student);
         bgParent = view.findViewById(R.id.bg_parent);
 
@@ -66,6 +84,7 @@ public class ClassInfoFragment extends Fragment {
         viewPager2 = view.findViewById(R.id.view_pager);
 
         toolbar = view.findViewById(R.id.toolbar);
+//        toolbar02 = view.findViewById(R.id.toolbar02);
         icSearch = view.findViewById(R.id.ic_toolbar_search);
         icSetting = view.findViewById(R.id.ic_toolbar_setting);
 
@@ -74,7 +93,9 @@ public class ClassInfoFragment extends Fragment {
         toolbar.setNavigationOnClickListener(v -> requireActivity().onBackPressed());
         setSearchToolbar(view);
         setMenuItemSelected();
-
+        SharedPreferences preferences = requireContext().getSharedPreferences("CLASSROOM_ID", Context.MODE_PRIVATE);
+        String name = preferences.getString("classroomName", "");
+        toolbar.setTitle(name);
         setViewPager();
         setBottomNav();
     }
@@ -122,17 +143,42 @@ public class ClassInfoFragment extends Fragment {
     private void setMenuItemSelected() {
 
         icSearch.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-                circleReveal(R.id.searchtoolbar, 1, true, true);
-            else
-                searchToolbar.setVisibility(View.VISIBLE);
+            circleReveal(R.id.searchtoolbar, 1, true, true);
 
             item_search.expandActionView();
         });
 
-        icSetting.setOnClickListener(v -> {
-            Toast.makeText(getContext(), "Home Settings Click", Toast.LENGTH_SHORT).show();
-        });
+        registerForContextMenu(icSetting);
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        requireActivity().getMenuInflater().inflate(R.menu.menu_class_student, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.xoalop) {
+            deleteClassRoom();
+        }
+        if (item.getItemId() == R.id.doiKi) {
+            broadCast(studentDetailLis01);
+        }
+        if (item.getItemId() == R.id.doiKi2) {
+            broadCast(studentDetailLis02);
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    void broadCast(List<StudentDetail> list) {
+        Intent intent = new Intent("ACTION");
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Array", (Serializable) list);
+        intent.putExtras(bundle);
+        LocalBroadcastManager.getInstance(requireActivity()).sendBroadcast(intent);
     }
 
     private void setSearchToolbar(@NonNull View view) {
@@ -144,10 +190,7 @@ public class ClassInfoFragment extends Fragment {
             searchToolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-                        circleReveal(R.id.searchtoolbar, 1, true, false);
-                    else
-                        searchToolbar.setVisibility(View.GONE);
+                    circleReveal(R.id.searchtoolbar, 1, true, false);
                 }
             });
 
@@ -157,10 +200,10 @@ public class ClassInfoFragment extends Fragment {
                 @Override
                 public boolean onMenuItemActionCollapse(MenuItem item) {
                     // Do something when collapsed
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        circleReveal(R.id.searchtoolbar, 1, true, false);
-                    } else
-                        searchToolbar.setVisibility(View.GONE);
+                    circleReveal(R.id.searchtoolbar, 1, true, false);
+                    Intent intent = new Intent("SEARCH");
+                    intent.putExtra("search", "");
+                    LocalBroadcastManager.getInstance(requireActivity()).sendBroadcast(intent);
                     return true;
                 }
 
@@ -203,27 +246,28 @@ public class ClassInfoFragment extends Fragment {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 callSearch(query);
-                searchView.clearFocus();
+                Intent intent = new Intent("SEARCH");
+                intent.putExtra("search", query);
+                LocalBroadcastManager.getInstance(requireActivity()).sendBroadcast(intent);
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 callSearch(newText);
+                Log.e(">>>>>>>", "onQueryTextChange: " + newText);
                 return true;
             }
 
             public void callSearch(String query) {
                 //Do searching
                 Log.i("query", "" + query);
-
             }
 
         });
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void circleReveal(int viewID, int posFromRight, boolean containsOverflow, final boolean isShow) {
         final View myView = requireView().findViewById(viewID);
 
@@ -262,5 +306,63 @@ public class ClassInfoFragment extends Fragment {
 
         // start the animation
         anim.start();
+    }
+
+    private void getListStudent() {
+        SharedPreferences preferences = requireContext().getSharedPreferences("CLASSROOM_ID", Context.MODE_PRIVATE);
+        int classroomId = preferences.getInt("classroomId", 0);
+
+        Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).getListStudentByIdClassRoom(classroomId);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 200) {
+                    Type type = new TypeToken<List<StudentDetail>>() {
+                    }.getType();
+                    studentDetailList = new ArrayList<>();
+                    studentDetailLis01 = new ArrayList<>();
+                    studentDetailLis02 = new ArrayList<>();
+                    studentDetailList = new Gson().fromJson(response.body().getAsJsonArray("data"), type);
+                    for (StudentDetail o : studentDetailList) {
+                        if (o.getSemester() % 2 != 0) {
+                            studentDetailLis01.add(o);
+                        } else {
+                            studentDetailLis02.add(o);
+                        }
+                    }
+
+
+                } else {
+                    Toast.makeText(requireContext(), "Lớp học chưa có học sinh", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(requireContext(), "Lỗi kết nối tới máy chủ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void deleteClassRoom() {
+        SharedPreferences preferences = requireContext().getSharedPreferences("CLASSROOM_ID", Context.MODE_PRIVATE);
+        int classroomId = preferences.getInt("classroomId", 0);
+        Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).deleteClassRoomById(classroomId);
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.code() == 204) {
+                    Toast.makeText(requireContext(), "Xóa lớp thành công", Toast.LENGTH_SHORT).show();
+                    Navigation.findNavController(requireActivity(), R.id.nav_host_teacher).navigate(R.id.action_classInfoFragment_to_viewClassFragment);
+                } else {
+                    Toast.makeText(requireContext(), "Không tìm thấy thông tin lớp", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(requireContext(), "Lỗi kết nối tới máy chủ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
