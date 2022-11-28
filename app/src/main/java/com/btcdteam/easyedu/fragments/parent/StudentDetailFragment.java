ae82ee148f490a12111f5cd3b3b1f8d346b7f2c7
suhdo <dungvipnr@gmail.com>
2022-11-28 11:09:31 +0700

    feat: add btn notification in StudentDetailFragment

--- app/src/main/java/com/btcdteam/easyedu/fragments/parent/StudentDetailFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/parent/StudentDetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDetailFragment extends Fragment {
-    private ImageView btnBack;
+    private ImageView btnBack, btnNoti;
     private TextView tvStudentName,
             tvStudentDateOfBirth,
             tvStudentGender,
@@ -51,6 +52,7 @@ public class StudentDetailFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btnBack = view.findViewById(R.id.img_parent_student_detail_back);
+        btnNoti = view.findViewById(R.id.img_parent_student_detail_noti);
         //student
         tvStudentName = view.findViewById(R.id.tv_student_detail_name);
         tvStudentDateOfBirth = view.findViewById(R.id.tv_student_detail_dob);
@@ -71,6 +73,11 @@ public class StudentDetailFragment extends Fragment {
 
         btnBack.setOnClickListener(v -> {
             requireActivity().onBackPressed();
+        });
+
+        btnNoti.setOnClickListener(v -> {
+            // chuyển đến màn hình thông báo
+            Navigation.findNavController(requireActivity(), R.id.nav_host_parent).navigate(R.id.action_studentDetailFragment2_to_notificationFragment);
         });
 
         swTerm.setOnCheckedChangeListener((buttonView, isChecked) -> {
