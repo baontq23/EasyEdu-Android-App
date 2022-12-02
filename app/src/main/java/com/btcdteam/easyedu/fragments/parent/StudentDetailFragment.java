d7bc51464fee3408f3a61953846c424f2fe3f052
baontq23 <baooshacker@gmail.com>
2022-12-03 04:30:29 +0700

    feat: parent activity

--- app/src/main/java/com/btcdteam/easyedu/fragments/parent/StudentDetailFragment.java
+++ app/src/main/java/com/btcdteam/easyedu/fragments/parent/StudentDetailFragment.java
@@ -1,14 +1,6 @@
 package com.btcdteam.easyedu.fragments.parent;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +8,27 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import com.btcdteam.easyedu.R;
-import com.btcdteam.easyedu.adapter.teacher.ClassroomAdapter;
-import com.btcdteam.easyedu.models.Classroom;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-import java.util.ArrayList;
+import com.btcdteam.easyedu.R;
+import com.btcdteam.easyedu.apis.ServerAPI;
+import com.btcdteam.easyedu.network.APIService;
+import com.btcdteam.easyedu.utils.PreviewScore;
+import com.btcdteam.easyedu.utils.SnackbarUntil;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StudentDetailFragment extends Fragment {
     private ImageView btnBack, btnNoti;
@@ -34,12 +41,12 @@ public class StudentDetailFragment extends Fragment {
             tvMidtermScore,
             tvFinalScore,
             tvTotalScore,
-            Tvtotal,
             tvAvg,
             tvClassName,
             tvSubject,
             tvTeacherName;
     private Switch swTerm;
+    private List<PreviewScore> list;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -65,8 +72,6 @@ public class StudentDetailFragment extends Fragment {
         tvTotalScore = view.findViewById(R.id.tv_student_detail_total);
         tvAvg = view.findViewById(R.id.tv_student_detail_avg);
         swTerm = view.findViewById(R.id.sw_student_detail_term);
-        Tvtotal = view.findViewById(R.id.Tvtotal);
-
         tvClassName = view.findViewById(R.id.tv_student_detail_class);
         tvSubject = view.findViewById(R.id.tv_student_detail_subject);
         tvTeacherName = view.findViewById(R.id.tv_student_detail_teacher_name);
@@ -77,15 +82,80 @@ public class StudentDetailFragment extends Fragment {
 
         btnNoti.setOnClickListener(v -> {
             // chuyển đến màn hình thông báo
-            Navigation.findNavController(requireActivity(), R.id.nav_host_parent).navigate(R.id.action_studentDetailFragment2_to_notificationFragment);
+            Bundle bundle = new Bundle();
+            bundle.putString("student_id", getArguments().getString("student_id"));
+            Navigation.findNavController(requireActivity(), R.id.nav_host_parent).navigate(R.id.action_studentDetailFragment2_to_notificationFragment, bundle);
         });
 
         swTerm.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked){
-                //set điểm kì 2
-            }else{
-                //set điểm kì 1
+            if (isChecked) {
+                toggleSemester(list.get(1));
+            } else {
+                toggleSemester(list.get(0));
             }
         });
+        initData();
+    }
+
+    private void initData() {
+        if (getArguments() != null) {
+            String studentId = getArguments().getString("student_id");
+            String classId = getArguments().getString("classroom_id");
+            Call<JsonObject> call = ServerAPI.getInstance().create(APIService.class).getScoreByStudentAndClass(studentId, classId);
+            call.enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    if (response.code() == 200) {
+                        Type type = new TypeToken<List<PreviewScore>>() {
+                        }.getType();
+                        list = new Gson().fromJson(response.body().getAsJsonArray("data"), type);
+                        toggleSemester(list.get(0));
+                        showTotalScore(list.get(0), list.get(1));
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+                    t.printStackTrace();
+                    SnackbarUntil.showError(requireView(), getString(R.string.server_connect_error));
+                }
+            });
+        } else {
+            SnackbarUntil.showError(requireView(), "Something went wrong!");
+        }
+    }
+
+    private void toggleSemester(PreviewScore score) {
+        Float avg = null;
+        if (score.regular_score_1 != null && score.regular_score_2 != null && score.regular_score_3 != null && score.midterm_score != null && score.final_score != null) {
+            avg = calcAvg(score);
+        }
+        tvTeacherName.setText(score.teacher_name);
+        tvSubject.setText(score.classroom_subject);
+        tvStudentName.setText(score.student_name);
+        tvStudentDateOfBirth.setText(score.student_dob);
+        tvStudentGender.setText(score.student_gender);
+        tvRegularScore1.setText(score.regular_score_1 == null ? "?" : String.format(Locale.US, "%.2f", score.regular_score_1));
+        tvRegularScore2.setText(score.regular_score_2 == null ? "?" : String.format(Locale.US, "%.2f", score.regular_score_2));
+        tvRegularScore3.setText(score.regular_score_3 == null ? "?" : String.format(Locale.US, "%.2f", score.regular_score_3));
+        tvMidtermScore.setText(score.midterm_score == null ? "?" : String.format(Locale.US, "%.2f", score.midterm_score));
+        tvFinalScore.setText(score.final_score == null ? "?" : String.format(Locale.US, "%.2f", score.final_score));
+        tvAvg.setText(avg == null ? "?" : String.format(Locale.US, "%.2f", avg));
+    }
+
+    private Float calcAvg(PreviewScore score) {
+        if (score.regular_score_1 != null && score.regular_score_2 != null && score.regular_score_3 != null && score.midterm_score != null && score.final_score != null) {
+            return ((score.regular_score_1 + score.regular_score_2 + score.regular_score_3 + (2 * (score.midterm_score)) + (3 * (score.final_score)))) / 8;
+        }
+        return null;
+    }
+
+    private void showTotalScore(PreviewScore score1, PreviewScore score2) {
+        if (calcAvg(score1) != null && calcAvg(score2) != null) {
+            Float total = (calcAvg(score1) + 2 * calcAvg(score2)) / 3;
+            tvTotalScore.setText(String.format(Locale.US, "%.2f", total));
+        } else {
+            tvTotalScore.setText("?");
+        }
     }
 }
